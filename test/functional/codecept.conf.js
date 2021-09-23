4664bb05480fcf3c9dccd51b766906495df37607
Jakub Kozłowski <jakub.kozlowski@gmail.com>
2021-09-23 14:35:58 +0100

    Reverting changes to functional tests config

--- test/functional/codecept.conf.js
+++ test/functional/codecept.conf.js
@@ -1,14 +1,10 @@
 exports.config = {
   tests: './*_test.js',
   output: './output',
-  timeout: 10000,
   helpers: {
     Puppeteer: {
-      show: true,
       url: 'http://localhost:3000',
-      waitForNavigation: ['load', 'domcontentloaded', 'networkidle0'],
-      waitForAction: 5000,
-      waitForTimeout: 10000,
+      waitForNavigation: ['domcontentloaded'],
       chrome: { ignoreHTTPSErrors: true }
     }
   },
