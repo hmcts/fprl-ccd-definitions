acbbad7b44d7b44dbc61689f0a4348bd62b62e3c
Patryk Jar <patryk.jar@hmcts.net>
2021-09-27 14:49:14 +0200

    resolve conflicts

--- test/end-to-end/steps_file.js
+++ test/end-to-end/steps_file.js
@@ -3,9 +3,6 @@ const LoginPage = require('./pages/Login');
 const PeopleInTheCasePage = require('./pages/PeopleInTheCase');
 const UploadDocuments = require('./pages/UploadDocuments');
 
-const { waitForPage } = require('./pages/common/common');
-const { createCase } = require('./pages/create-case/create-case');
-const { addNewDocument, uploadDocuments } = require('./pages/upload-documents/upload-documents');
 const { goingToCourtSelectNoForAll, goingToCourtSelectYesForAll } = require('./pages/going-to-court/goint-to-court');
 const generalHelper = require('./helpers/generalHelper');
 
@@ -17,10 +14,6 @@ module.exports = () => {
     loginAsSolicitor() {
       return LoginPage.loginAsSolicitor();
     },
-    createCase,
-    waitForPage,
-    addNewDocument,
-    uploadDocuments,
     goingToCourtSelectNoForAll,
     goingToCourtSelectYesForAll,
 
